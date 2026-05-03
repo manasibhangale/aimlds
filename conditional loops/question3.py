87ae96d3f117bb41dc0dd86e9c42a12639fea590
manasibhangale <manasibhangale2004@gmail.com>
2026-05-03 11:33:42 +0530

    check if a number is positive negative or zero

---
+++ conditional loops/question3.py
@@ -0,0 +1,7 @@
+num=float(input("enter a number:"))
+if num>0:
+    print(f"{num} is a positive number.")
+elif num<0:
+    print(f"{num} is a negative number.")
+else:
+    print(f"{num} is zero.")
