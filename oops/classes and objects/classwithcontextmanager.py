bb18115f9c36c195140e57417a5dafcb8271f367
manasibhangale <manasibhangale2004@gmail.com>
2026-05-07 18:08:07 +0530

    class with context manager

---
+++ oops/classes and objects/classwithcontextmanager.py
@@ -0,0 +1,16 @@
+class FileManager:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.file.close()
+
+# Test
+with FileManager('sample.txt', 'r') as file:
+    content = file.read()
+    print(content)
