2eb1832075bf092f39fa8e75d056781716f51459
manasibhangale <manasibhangale2004@gmail.com>
2026-05-07 15:48:46 +0530

    reading a file that is not found

---
+++ exception handling/filenotfound.py
@@ -0,0 +1,13 @@
+def read_a_file(filename):
+    try:
+        file=open(filename,'r')
+        content=file.read()
+        print(content)
+    except FileNotFoundError as ex:
+        print(f"error {ex}")
+    finally:
+        try:
+            file.close()
+        except NameError:
+            pass
+read_a_file("demo.txt")
