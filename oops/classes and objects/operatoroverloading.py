d37e32cb3085705c22bbbde13c23391f24e84d21
manasibhangale <manasibhangale2004@gmail.com>
2026-05-07 18:04:54 +0530

    operator overloading

---
+++ oops/classes and objects/operatoroverloading.py
@@ -0,0 +1,19 @@
+### Assignment 12: Operator Overloading
+
+#Create a class named `Vector` with attributes `x` and `y`. Overload the `+` operator to add two `Vector` objects. Create objects of the class and test the operator overloading.
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+# Test
+v1 = Vector(2, 3)
+v2 = Vector(4, 5)
+v3 = v1 + v2
+print(v3)  # Vector(6, 8)
