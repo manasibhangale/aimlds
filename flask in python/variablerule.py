14c9fa9952840b1b384b9d421c18d86f5386d6b6
manasibhangale <manasibhangale2004@gmail.com>
2026-05-12 16:47:07 +0530

    variable rule

---
+++ flask in python/variablerule.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+def welcome():
+    return "welcome to homepage"
+@app.route("/index")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/score/<int:marks>")
+def score(marks):
+    return "the score is: " +str(marks)
+if __name__=="__main__":
+    app.run(debug="True")
